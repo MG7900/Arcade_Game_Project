2cf2f0711ae92703030838f6927ec1b74aeb43c5
MikeG <MikeGao2024@outlook.com>
2026-04-03 07:25:55 -0400

    added key listener

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -26,34 +26,34 @@ import javax.swing.JPanel;
 //*******************************************************************************
 // Class Definition Section
 
-public class BasicGameApp implements Runnable, MouseListener {
+public class BasicGameApp implements Runnable, MouseListener, KeyListener {
     //todo: add KeyListener
 
-   //Variable Definition Section
-   //Declare the variables used in the program 
-   //You can set their initial values too
-   
-   //Sets the width and height of the program window
-	final int WIDTH = 1000;
-	final int HEIGHT = 700;
+    //Variable Definition Section
+    //Declare the variables used in the program
+    //You can set their initial values too
 
-   //Declare the variables needed for the graphics
-	public JFrame frame;
-	public Canvas canvas;
-   public JPanel panel;
+    //Sets the width and height of the program window
+    final int WIDTH = 1000;
+    final int HEIGHT = 700;
 
-   public Rectangle startHitbox;
-   
-	public BufferStrategy bufferStrategy;
+    //Declare the variables needed for the graphics
+    public JFrame frame;
+    public Canvas canvas;
+    public JPanel panel;
+
+    public Rectangle startHitbox;
+
+    public BufferStrategy bufferStrategy;
 
     public Image ninjaPic;
     public Image Freeze_BuffPic;
     public Image KunaiPic;
     public Image ShurikenPic;
 
-   //Declare the objects used in the program
-   //These are things that are made up of more than one variable type
-	public Ninja ninja;
+    //Declare the objects used in the program
+    //These are things that are made up of more than one variable type
+    public Ninja ninja;
     public Kunai kunai;
     public Shuriken shuriken;
     public Freeze_Buff freeze_buff;
@@ -62,124 +62,123 @@ public class BasicGameApp implements Runnable, MouseListener {
 
     public boolean mouseheld;
 
-   // Main method definition
-   // This is the code that runs first and automatically
-	public static void main(String[] args) {
-		BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
-		new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method  
-	}
+    // Main method definition
+    // This is the code that runs first and automatically
+    public static void main(String[] args) {
+        BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
+        new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
+    }
 
 
-   // Constructor Method
-   // This has the same name as the class
-   // This section is the setup portion of the program
-   // Initialize your variables and construct your program objects here.
-	public BasicGameApp() {
-      
-      setUpGraphics();
-       
-      //variable and objects
-      //create (construct) the objects needed for the game and load up 
-		ninjaPic = Toolkit.getDefaultToolkit().getImage("Ninja.png"); //load the picture
+    // Constructor Method
+    // This has the same name as the class
+    // This section is the setup portion of the program
+    // Initialize your variables and construct your program objects here.
+    public BasicGameApp() {
+
+        setUpGraphics();
+
+        //variable and objects
+        //create (construct) the objects needed for the game and load up
+        ninjaPic = Toolkit.getDefaultToolkit().getImage("Ninja.png"); //load the picture
         ShurikenPic = Toolkit.getDefaultToolkit().getImage("Shuriken.png");
-        KunaiPic= Toolkit.getDefaultToolkit().getImage("Kunai.png");
+        KunaiPic = Toolkit.getDefaultToolkit().getImage("Kunai.png");
         Freeze_BuffPic = Toolkit.getDefaultToolkit().getImage("Freeze_Buff.png");
 
-        startHitbox = new Rectangle(100,100,100,100);
+        startHitbox = new Rectangle(100, 100, 100, 100);
 
         start_game = false;
 
         mouseheld = false;
 
-		ninja = new Ninja(10,100);
+        ninja = new Ninja(10, 100);
         //todo: do the same for other game elements
 
 
-	}// BasicGameApp()
+    }// BasicGameApp()
 
-   
+
 //*******************************************************************************
 //User Method Section
 //
 // put your code to do things here.
 
-   // main thread
-   // this is the code that plays the game after you set things up
-	public void run() {
+    // main thread
+    // this is the code that plays the game after you set things up
+    public void run() {
 
-      //for the moment we will loop things forever.
-		while (true) {
+        //for the moment we will loop things forever.
+        while (true) {
 
-         moveThings();  //move all the game objects
-         render();  // paint the graphics
-         pause(20); // sleep for 10 ms
-		}
-	}
+            moveThings();  //move all the game objects
+            render();  // paint the graphics
+            pause(20); // sleep for 10 ms
+        }
+    }
 
 
-	public void moveThings()
-	{
-      //calls the move( ) code in the objects
-		if(start_game == true) {
+    public void moveThings() {
+        //calls the move( ) code in the objects
+        if (start_game == true) {
             ninja.move();
         }
-	}
-	
-   //Pauses or sleeps the computer for the amount specified in milliseconds
-   public void pause(int time ){
-   		//sleep
-			try {
-				Thread.sleep(time);
-			} catch (InterruptedException e) {
+    }
 
-			}
-   }
+    //Pauses or sleeps the computer for the amount specified in milliseconds
+    public void pause(int time) {
+        //sleep
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
 
-   //Graphics setup method
-   private void setUpGraphics() {
-      frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
-   
-      panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
-      panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
-      panel.setLayout(null);   //set the layout
-   
-      // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
-      // and trap input events (Mouse and Keyboard events)
-      canvas = new Canvas();  
-      canvas.setBounds(0, 0, WIDTH, HEIGHT);
-      canvas.setIgnoreRepaint(true);
-   
-      panel.add(canvas);  // adds the canvas to the panel.
-   
-      // frame operations
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
-      frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
-      frame.setResizable(false);   //makes it so the frame cannot be resized
-      frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
-      
-      // sets up things so the screen displays images nicely.
-      canvas.createBufferStrategy(2);
-      bufferStrategy = canvas.getBufferStrategy();
-      canvas.requestFocus();
-      System.out.println("DONE graphic setup");
-   
-   }
+        }
+    }
+
+    //Graphics setup method
+    private void setUpGraphics() {
+        frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+        panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+        panel.setLayout(null);   //set the layout
+
+        // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+        // and trap input events (Mouse and Keyboard events)
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);  // adds the canvas to the panel.
+
+        // frame operations
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+        frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+        frame.setResizable(false);   //makes it so the frame cannot be resized
+        frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+        // sets up things so the screen displays images nicely.
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+
+    }
 
 
-	//paints things on the screen using bufferStrategy
-	private void render() {
+    //paints things on the screen using bufferStrategy
+    private void render() {
         //todo: add keylistener and mouselistener etc
         //todo: render other elements
 
-		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-		g.clearRect(0, 0, WIDTH, HEIGHT);
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
 
         //todo: make a better start button
 
         g.setColor(Color.blue);
-        g.fillRect(400,400,200,50);
+        g.fillRect(400, 400, 200, 50);
 
-        if(start_game == true) {
+        if (start_game == true) {
             //draw the image of the ninja
             g.drawImage(ninjaPic, ninja.xpos, ninja.ypos, 100, 100, null);
 
@@ -188,17 +187,17 @@ public class BasicGameApp implements Runnable, MouseListener {
             g.drawImage(ShurikenPic, shuriken.xpos, shuriken.ypos, shuriken.width, shuriken.height, null);
 
         }
-		g.dispose();
+        g.dispose();
 
-		bufferStrategy.show();
-	}
+        bufferStrategy.show();
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         mouseheld = true;
         System.out.println(e.getPoint());
-        Rectangle mouseHitbox = new Rectangle(e.getX(),e.getY(),1,1);
-        if(startHitbox.intersects(mouseHitbox)){
+        Rectangle mouseHitbox = new Rectangle(e.getX(), e.getY(), 1, 1);
+        if (startHitbox.intersects(mouseHitbox)) {
             System.out.println("Game starts!");
             start_game = true;
         }
@@ -222,5 +221,56 @@ public class BasicGameApp implements Runnable, MouseListener {
     @Override
     public void mouseExited(MouseEvent e) {
 
+    }
+
+    //Key portion below
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e.getKeyChar());
+        //gets the letters that were pressed, not e.g. backspace or space
+
+        //a code for each key is different, unique so
+
+//        if(e.getKeyCode() == 39){ //when up arrow is pressed
+//
+//            astro.dx = astro.dx - 1;
+//
+//            //the below code makes it always go up every time pressed up button
+//            // astro.dy = -Math.abs(astro.dy);
+//        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {   //key released is triggered everytime we stop touching a key
+        System.out.println("I stopped touching " + e.getKeyCode());
+        //to see which key code we jsut released
+
+//        if(e.getKeyCode() == 38 ){  //38 is up arrow
+//            astro.isNorth = false;
+//            System.out.println("not going up");
+//        }
+//        if(e.getKeyCode() == 40 ){
+//            System.out.println("not going down");
+//
+//            astro.isSouth = false;
+//        }
+//        if(e.getKeyCode() == 39 ){
+//            System.out.println("not going east");
+//
+//            astro.isEast = false;
+//        }
+//        if(e.getKeyCode() == 37 ){
+//            System.out.println("not going west");
+//
+//            astro.isWest = false;
+//        }
+//
+//    }
     }
 }
